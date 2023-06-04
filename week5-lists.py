bc1d4fa6f5426dea739019269b58737a0bcebc33
sdcloud92 <shomarri.diaz@gmail.com>
2023-06-05 00:44:12 +0100

    week 5 class, learning how use create and manipulate lists

---
+++ week5-lists.py
@@ -0,0 +1,77 @@
+# LAB 1 - CREATING OUR FIRST LIST
+
+Anime = ["Naruto", "Shingeki No Kyojin", "Bleach", "Yu Yu Hakusho", "Dragonball Z"]
+print(Anime)
+
+Martial_arts = ["Boxing", "Wrestling", "Muaythai", "Jiu-jitsu", "Krav Maga"]
+print(Martial_arts)
+
+Asian_celebs = ["Kim Go-eun", "Choi Seo-eun", "Zhang Ziyi", "Satomi Ishihara"]
+print(Asian_celebs)
+
+Asia = ["Japan", "South Korea", "Phillipines", "Thailand", "Khazakstan"]
+print(Asia)
+
+Lanaguages_I_speak = ["English", "Chinese", "Korean", "Japanese", "Spanish"]
+print(Lanaguages_I_speak)
+
+# LAB 2 PRINTING OUT A SPECIFIC ELEMENT OR VALUE FROM THIS LIST
+
+Cars = ["Tesla", "BWM", "Nissan GTR", "Ferrari", "Ford"]
+print(Cars[4])
+
+Favorite_Female_Actress = ["Stacy Dash", "Halle Berry", "Vivica Fox", "Nia Long", "Rosie Perez", "Eva Mendes"]
+print(Favorite_Female_Actress[-4])
+
+Anime = ["Naruto", "Shingeki No Kyojin", "Bleach", "Yu Yu Hakusho", "Dragonball Z"]
+print(Anime[0])
+
+Martial_arts = ["Boxing", "Wrestling", "Muay thai", "Jiu-jitsu", "Krav Maga"]
+print(Martial_arts[3])
+
+Asian_celebs = ["Kim Go-eun", "Choi Seo-eun", "Zhang Ziyi", "Satomi Ishihara"]
+print(Asian_celebs[-3])
+
+Asia = ["Japan", "South Korea", "Phillipines", "Thailand", "Khazakstan"]
+print(Asia[1])
+
+Lanaguages_I_speak = ["English", "Chinese", "Korean", "Japanese", "Spanish"]
+print(Lanaguages_I_speak[-2])
+
+# LAB 3 PRINTING MULTIPLE ELEMENTS FROM YOUR LIST
+
+Laptops = ["Lenovo", "Apple", "HP", "Dell", "Samsung"]
+print(Laptops[0:3])
+
+Anime = ["Naruto", "Shingeki No Kyojin", "Bleach", "Yu Yu Hakusho", "Dragonball Z"]
+print(Anime[0:2])
+
+Martial_arts = ["Boxing", "Wrestling", "Muaythai", "Jiu-jitsu", "Krav Maga"]
+print(Martial_arts[0:4])
+
+Asian_celebs = ["Kim Go-eun", "Choi Seo-eun", "Zhang Ziyi", "Satomi Ishihara"]
+print(Asian_celebs[0:1])
+
+Asia = ["Japan", "South Korea", "Phillipines", "Thailand", "Khazakstan"]
+print(Asia[2:4])
+
+Lanaguages_I_speak = ["English", "Chinese", "Korean", "Japanese", "Spanish"]
+print(Lanaguages_I_speak[1:3])
+
+# LAB 4 MODIFYING A VALUE IN YOUR LIST AND UPDATING IT
+
+Anime = ["Naruto", "Shingeki No Kyojin", "Bleach", "Yu Yu Hakusho", "Dragonball Z"]
+Anime[2] = "Samurai Champloo"
+print(Anime[2])
+
+Asian_celebs = ["Kim Go-eun", "Choi Seo-eun", "Zhang Ziyi", "Satomi Ishihara"]
+Asian_celebs[3] = "Song Hye-kyo"
+print(Asian_celebs) 
+
+Asia = ["Japan", "South Korea", "Phillipines", "Thailand", "Khazakstan"]
+Asia[4] = "China"
+print(Asia)
+
+Lanaguages_I_speak = ["English", "Chinese", "Korean", "Japanese", "Spanish"]
+Lanaguages_I_speak[0] = "Russian"
+print(Lanaguages_I_speak)
