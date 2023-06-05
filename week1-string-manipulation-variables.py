23a191ac1d5dd2ef31f2faf0a3b583d2d4eca348
pythonclassip23 <pythonclassip@gmail.com>
2023-06-05 09:07:44 +0100

    week1 first Python lesson with IP, Introduction to strings and variables

---
+++ week1-string-manipulation-variables.py
@@ -0,0 +1,32 @@
+#This Lesson is all about strings (Plain text data type)
+#Print function = Plain text data
+
+#LAB1: print("New\nline")
+#Examples
+print("\"Bruh!\" Allen Iversion is the greatest baller ever")
+print("The best Brazilian movie of all time is ""\"City of God\".")
+
+#Lab2: Variables (Containers to store data values)
+my_str = "Can you keep a secret? I love ""\"Korean\" food!"
+print(my_str)
+
+my_str_2 = "I need Help!\nI think I have an obsession!...\n""\"I love Korean BBQ\""
+print(my_str_2)
+
+
+
+#Lab 3" Variable continued
+#Example of how to use variables to deploy changes to lots of code
+character_name = "Romell"
+character_age = "30"
+profession = "DevOps Cloud Engineer"
+women = "Korean"
+character_country = "Korea"
+wife = "지수" 
+
+print("There is a man called " +character_name+",")
+print("He is "  +character_age+  " years old"",")
+print("He worked hard and became a " +profession+ ",")
+print("He dreamed of travelling to South Korea to find a " +women+ " ""wife"".")
+print("After working hard, investing and saving his money, Romell moved to " +character_country+ ".")
+print("He lived his best life, became a millionaire, dated many beautiful " +women+ " ""women"". " "Then eventually settled down and married a gorgeous woman named " +wife+".")
