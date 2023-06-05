8cefbc103eebee99fb22da2fb8ff8a285025849f
pythonclassip23 <pythonclassip@gmail.com>
2023-06-05 08:58:11 +0100

    Week2 class, strings and variables

---
+++ week2-strings-variables.py
@@ -0,0 +1,70 @@
+#20 Lines of code for strings
+print("I'm so tired!")
+print("I am tired because I have slept only five hours in the last two days")
+print("I have had so little sleep because I was studying for my Terraform exam and had three interviews and conversations with recruiters")
+print("The last three weeks have been great, I joined IP's ""\"International Passport Bros\" network")
+print("We are the ""\"Passport Bros\". We have a strong community of ""\"Professional\" ""\"black men\".")
+print("Our mission is to encourage more ""\"black men\" to pursue careers in ""\"IT\" to achieve ""\"financial\" and ""\"geographical\" freedom.")
+print("There are so many countries that I want to visit and to live in")
+print("I am currently making a list of my top countries to visit vs to live in")
+print("The countries I want to live in are:\nJapan\nSouth Korea\nColombia")
+print("The first country I moved to was\nChina\nI lived in a province called ""\"Shandong\"")
+print("I am at work right now writing my ""\"Python\" homework for ""\"IP\".")
+print("It's now 05:30am and I just called Andrewski to teach him what we learned in IP's ""\"Week Two\" Python class.")
+print("I watched IP's ""\"Scandinavian\" videos and now I think I need to travel to:\nSweden\nNorway\n& Denmark")
+print("When I finish work I am going straight to sleep I am knackered.")
+print("Running out of things to say but I need to keep practising writing my ""\"print\" functions.")
+print("I have been studying so much, I have been slacking on my fitness training...\n""\"FFS\"")
+print("Wooo! The last print line of code done!")
+
+#20 Lines of code for variables
+old_job = "Home Office"
+hobby = "travelling"
+profession1 = "English teacher"
+profession2 = "DevOps Engineer"
+asia1 = "China"
+asia2 = "South Korea"
+country = "UK"
+problem = "pandemic"
+best_women = "Korean women"
+
+print("Three and a half years ago I worked a corporate job at the "+old_job+".")
+print("I really hated that job at the "+old_job+", and I dreamed of "+hobby+".")
+print("Before working at the "+old_job+" I had not done any "+hobby+" In over ""\"ten years\"!")
+print("While at work one day, I came across a Youtube video about being an "+profession1+" In "+asia1+".")
+print("At that point the light bulb went off in my mind and saw a way to escape the "+country+". I immediately began training to become an "+profession1+".")
+print("A year later I finally left the "+country+", I still remember the joy,nervousness and excitement the first time I landed in "+asia1+".")
+print("I made many new friends in "+asia1+". I knew straight away I was going to love my new life of "+hobby+".")
+print("Unfortunately my life in "+asia1+" would not last long because of the "+problem+".")
+print("The "+problem+" started in "+asia1+" December 2019. Being on lock down in "+asia1+" was not fun at all!")
+print("Because of the "+problem+" I left China and came back to the "+country+" and quickly became depressed.")
+print("Ten months later, I had an opportunity to return to "+asia1+". However, there were no direct flights to China,\nInstead, I first had to land in "+asia2+" and Quarantine for ""\"two weeks!\".")
+print("Prior to this I had little to no knowledge about "+best_women+".")
+print("Lucky for me, "+asia1+" cancelled all British peoples Visas because the "+problem+" had reached the "+country+" which had then just locked down.")
+print("Because I could work remotely, I decided to stay in "+asia2+" which ended up being the best decision I ever made in my life. My life of "+hobby+" was back on.")
+print("My view on dating completely changed once I began dating "+best_women+". Wow I never knew what it meant for a man to be treated like a ""\"King\".")
+print("I have a plan in place to get back to my life of "+hobby+".")
+print("I am working hard to land a role as a "+profession2+".")
+print("Working as a "+profession2+" will allow me to work remotely. With this geographical freedom I will be "+hobby+" to multiple countries,\nseveral times a year.")
+
+#20 lines of comments
+#한눈팔지 말 것, 절대 방심하지 말 것
+#한눈팔지 말 것
+#절대 방심하지 말 것
+#책임지고 있는 사람들을 꼭 기억할 것
+#더 힘을 가질 것
+#더 많은 돈을 가질 것
+#어깨가 무거워도 절대 무너지지 말 것
+#한눈팔지 말 것
+#절대 방심하지 말 것
+#책임지고 있는 사람들을 꼭 기억할 것
+#더 힘을 가질 것
+#더 많은 돈을 가질 것
+#어깨가 무거워도 절대 무너지지 말 것
+#절대 힘들지 말 것
+#절대 흐트러지지 말 것
+#항상 강하게 우뚝 있던 자리에 서 있을 것
+#다 바닥을 볼 때
+#나는 위를 향하고 있을 것
+#그때 너가 위를 볼 때
+#그땐 우리가 정상에 있을걸
